89b2dcbfac18db17af8bb3675376035ec4bf341e
Matt Landreman <mattland@umd.edu>
2022-12-23 13:53:58 -0500

    Added HSX coils

--- src/CoilForces.jl
+++ src/CoilForces.jl
@@ -5,11 +5,13 @@ export CurveCircle, CurveXYZFourier
 export γ, dγdϕ, d2γdϕ2, d3γdϕ3, curvature_torsion
 export Coil
 export analytic_force_per_unit_length
+export get_curve
 
 include("utils.jl")
 include("Curve.jl")
 include("CurveCircle.jl")
 include("CurveXYZFourier.jl")
+include("configs.jl")
 include("BiotSavart.jl")
 
 end
