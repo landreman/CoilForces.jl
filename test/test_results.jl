2e69dcc91eb18b9ac118501edf2f807f53c87295
Matt Landreman <mattland@umd.edu>
2023-04-30 10:24:14 -0400

    Forgot to commit another change

--- test/test_results.jl
+++ test/test_results.jl
@@ -13,4 +13,8 @@ using Test
     @testset "Test plot_force_non_convergence_skipping_point_circular_with_ours" begin
         CoilForces.plot_force_non_convergence_skipping_point_circular_with_ours()
     end
+
+    @testset "Test reproduce_Sienas_plot_of_locally_circular_approx" begin
+        CoilForces.reproduce_Sienas_plot_of_locally_circular_approx()
+    end
 end
