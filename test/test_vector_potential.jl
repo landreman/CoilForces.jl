c223ed5565fadd1ab5868ccc44cc43099313479c
Matt Landreman <mattland@umd.edu>
2023-05-28 15:34:54 -0400

    Added high-order terms to vector potential test

--- test/test_vector_potential.jl
+++ test/test_vector_potential.jl
@@ -15,20 +15,26 @@ using Test
             for jθ in 1:nθ
                 ρ = (jρ - 1) / (nρ - 1)
                 θ = 2π * (jθ - 1) / (nθ - 1)
-                r_eval = [R0 + aminor * ρ * cos(θ), 0, aminor * ρ * sin(θ)]
+                r_eval = [R0 - aminor * ρ * cos(θ), 0, aminor * ρ * sin(θ)]
                 A_hifi = CoilForces.A_finite_thickness(
                     coil, 
                     r_eval;
-                    reltol=1e-5,
+                    reltol=1e-6,
                     abstol=1e-12,
                 )
 
-                # For this analytic result, see the end of section 7 in
+                # For the leading-order analytic result, see the end of section 7 in
                 # 20230517-01 Self-inductance of a general-shape coil with circular cross-section.pdf
-                A_analytic = μ0 * current / (4π) * (-ρ^2 - 3 + 2 * log(8 * R0 / aminor))
+                # For the high-order analytic result, see eq (143) in
+                # 20230528-01_B_field_for_general_coil_with_circular_cross_section_coil_using_vector_potential.pdf
+                A_analytic = μ0 * current / (4π) * (
+                    -ρ^2 - 3 + 2 * log(8 * R0 / aminor)
+                    + (-3 - 0.25 * ρ^2 + log(8 * R0 / aminor)) * (aminor / R0) * ρ * cos(θ)
+                )
                 @test abs(A_hifi[1]) < 1e-8
                 @test abs(A_hifi[3]) < 1e-8
-                @test A_hifi[2] ≈ A_analytic rtol=5e-4
+                #@test A_hifi[2] ≈ A_analytic rtol=5e-4  # Looser rtol for high-order terms only
+                @test A_hifi[2] ≈ A_analytic rtol=4e-7
             end
         end
     end
